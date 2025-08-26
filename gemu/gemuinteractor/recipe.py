75b2e0be1b67a0679b5eb4cc20a54347700aaa99
T J <thorsten.jenke@fkie.fraunhofer.de>
2025-08-26 09:40:41 +0200

    added MSI support

--- gemu/gemuinteractor/recipe.py
+++ gemu/gemuinteractor/recipe.py
@@ -35,7 +35,7 @@ class Recipe:
         return text
 
     def _get_recipe_dict(self, sample: Path|str, export: str|None) -> dict:
-        filetype = subprocess.check_output(["file", sample]).decode("utf-8")
+        filetype = subprocess.check_output(["file", "-b", sample]).decode("utf-8")
         if "PE" in filetype:
             if export:
                 if "PE32+" in filetype: # 64bit Library
@@ -44,6 +44,8 @@ class Recipe:
                     return self.parse_yaml(RECIPE_FOLDER / "single_library_with_export_32bit.yml")
             else:
                 return self.parse_yaml(RECIPE_FOLDER / "single_native_binary.yml")
+        elif "MSI Installer" in filetype:
+            return self.parse_yaml(RECIPE_FOLDER / "single_msi_file.yml")
         raise NotImplementedError
 
     @staticmethod
