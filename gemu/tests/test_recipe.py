bca177a4e523df23a0f2a226764e429d950fdebb
T J <thorsten.jenke@fkie.fraunhofer.de>
2025-08-27 10:16:18 +0200

    added tests

--- gemu/tests/test_recipe.py
+++ gemu/tests/test_recipe.py
@@ -4,9 +4,11 @@ import pytest
 import yaml
 
 from gemuinteractor.config_parser import SAMPLE_NAME
-from gemuinteractor.recipe import Recipe, RECIPE_FOLDER
+from gemuinteractor.recipe import Recipe
 from tests.test_gemu_runner_single_file import USER, SMALL_BITNESS_PE, BIG_BITNESS_PE
 
+MSI_FILE = Path(__file__).parent / "testfiles" / "test.msi"
+LNK_FILE = Path(__file__).parent / "testfiles" / "test.lnk"
 
 def test_recipe_get_native_binary():
     recipe = Recipe(USER, SMALL_BITNESS_PE)
@@ -15,6 +17,22 @@ def test_recipe_get_native_binary():
     assert recipe.commands == ['start C:\\Users\\testuser\\Desktop\\ahsofi.exe']
     assert recipe.mountings == [(SMALL_BITNESS_PE, 'C:\\Users\\testuser\\Desktop\\ahsofi.exe')]
     assert recipe.requirements == {SMALL_BITNESS_PE}
+
+def test_recipe_get_msi_file():
+    recipe = Recipe(USER, MSI_FILE)
+
+    assert recipe.sample_name == "msiexec.exe"
+    assert recipe.commands == ['msiexec.exe /i C:\\Users\\testuser\\Desktop\\msiahsofi.msi']
+    assert recipe.mountings == [(MSI_FILE, 'C:\\Users\\testuser\\Desktop\\msiahsofi.msi')]
+    assert recipe.requirements == {MSI_FILE}
+
+def test_recipe_get_lnk_file():
+    recipe = Recipe(USER, LNK_FILE)
+
+    assert recipe.sample_name == "cmd.exe"
+    assert recipe.commands == ['start C:\\Users\\testuser\\Desktop\\ahsofilnk.lnk']
+    assert recipe.mountings == [(LNK_FILE, 'C:\\Users\\testuser\\Desktop\\ahsofilnk.lnk')]
+    assert recipe.requirements == {LNK_FILE}
 
 def test_recipe_get_native_binary_with_export():
     recipe = Recipe(USER, SMALL_BITNESS_PE, export="TestExport")
